2210dbf6427c6c9aa08ad83b1664a174872f1d5b
Ankan Basu <an.basu.kan@gmail.com>
2022-06-28 17:53:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar';
+import About from './Components/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  categories = ['General', 'Business', 'Entertainment', 'Health', 'Science', 'Sports', 'Technology'];
+
+  state = {
+    progress: 0
+  };
+  
+  apiKey = process.env.REACT_APP_API_KEY;
+
+
+  setProgress = (newProgress) => {
+    this.setState({
+      progress: newProgress
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Router>
+          <Navbar categories={this.categories} />
+          <LoadingBar
+            color='#f11946'
+            progress={this.state.progress}
+            onLoaderFinished={() => this.setProgress(0)}
+          />
+
+          <Routes>
+
+            <Route path='/' element={<About />} />
+            {this.categories.map((item) => {
+              return (
+                <Route path={`/${item.toLowerCase()}`} element={<News apiKey={this.apiKey} category={item} country="us" pageSize={9} setProgress={this.setProgress} key={item} />} key={item} />);
+            })}
+
+          </Routes>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
